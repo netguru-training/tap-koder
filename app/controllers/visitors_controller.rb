85f2eb1c378fdfe8a686e0017f7e1ee69829d060
Maciej Czuchnowski <mczuchnowski@users.noreply.github.com>
2014-07-19 12:32:08 +0200

    Index and Show for visitors

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,10 +2,10 @@ class VisitorsController < ApplicationController
   skip_before_filter :authenticate_user!
 
   def index
-
+    @offers = Offer.order("created_at DESC") # probbaly newest on top ;)
   end
 
   def show
-    
+    @offer = Offer.find(params[:id])
   end
 end
