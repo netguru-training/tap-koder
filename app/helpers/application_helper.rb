42e301614acabd91b12a45b909a932705bc16e9c
Szymon Chorobski <szymon.chorobski@gmail.com>
2014-07-19 16:50:06 +0200

    show offer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def publisher(offer)
+    ret = "#{link_to offer.user.name, "#"}"+
+    if signed_in? && offer.user == current_user
+      "<span class='info-success'>(you)</span>"
+    end
+    ret.html_safe
+  end
 end
