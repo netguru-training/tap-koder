18b8080ad7cb967d37730e7d78efedf6cd168d7a
Maciej <maciejradziwon1987@gmail.com>
2014-07-19 15:11:41 +0200

    Change description

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -73,6 +73,6 @@ class OffersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def offer_params
-      params.require(:offer).permit(:name, :destription, :user_id, :code_limit, :start_date)
+      params.require(:offer).permit(:name, :description, :user_id, :code_limit, :start_date)
     end
 end
