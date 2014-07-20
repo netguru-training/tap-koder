a8b22eedb7a0999dbe4d76cfc6bc853477392c75
Maciej <maciejradziwon1987@gmail.com>
2014-07-20 11:50:53 +0200

    Add validation to codes

--- app/models/code.rb
+++ app/models/code.rb
@@ -1,3 +1,8 @@
 class Code < ActiveRecord::Base
   belongs_to :offer
+  before_save { email.downcase! }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true,
+                             format: { with: VALID_EMAIL_REGEX },
+                             uniqueness: { case_sensitive: false }
 end
