08792c27333a63a293f824cfc8264f445c1f9c6e
Maciej Czuchnowski <mczuchnowski@users.noreply.github.com>
2014-07-20 10:37:22 +0200

    Add codes route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :offers
+  resources :offers do
+    resources :codes, only: [:create]
+  end
+
   root :to => "visitors#index"
   devise_for :users
 end
